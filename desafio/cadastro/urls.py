b13248c02c558b2e79e81fe283c733219d073ff0
jeffersonguanabara <jefferson.guanabara@dce.ufpb.br>
2020-03-03 16:50:30 -0300

    projeto concluido

--- desafio/cadastro/urls.py
+++ desafio/cadastro/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import *
-from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path('', Login.as_view(), name='login'),
-    path('cadastrar-cliente', login_required(CadastrarCliente.as_view()), name='cadastro-cliente'),
+    path('cadastrar-cliente', CadastrarCliente.as_view(), name='cadastro-cliente'),
     path('cadastrar-usuario', Usuario.as_view(), name='usuario'),
-    path('clientes', login_required(ListarCliente.as_view()), name='cliente'),
-    path('deletar-cliente', login_required(DeletarCliente.as_view()), name='deletar-cliente'),
-    path('editar-cliente', login_required(EditarCliente.as_view()), name="editar-cliente"),
+    path('clientes', ListarCliente.as_view(), name='cliente'),
+    path('deletar-cliente/<int:pk>/', DeletarCliente.as_view(), name='deletar-cliente'),
+    path('editar-cliente/<int:pk>/', EditarCliente.as_view(), name='editar-cliente'),
     path('logout', Logout.as_view(), name='logout')
 ]
--- desafio/cadastro/views.py
+++ desafio/cadastro/views.py
@@ -9,77 +9,98 @@ from django.http import JsonResponse
 
 # Create your views here.
 
-
 class ListarCliente(View):
-
+    
     def get(self, request):
         context = {}
-        try: 
-            clientes = Cliente.objects.all()
-            context['clientes'] = list(clientes)
-        except:
-            context['clientes'] = []
-        return render(request, 'cliente/listagem.html', context)
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            try: 
+                clientes = Cliente.objects.all()
+                context['clientes'] = list(clientes)
+            except:
+                context['clientes'] = []
+            return render(request, 'cliente/listagem.html', context)
 
 
 class CadastrarCliente(View):
-
+    
     def get(self, request):
-        return render(request, 'cliente/cadastrar-cliente.html')
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            return render(request, 'cliente/cadastrar-cliente.html')
 
     def post(self, request):
-        print('entrou no cadastrar cliente')
-        context = {}
-        teste = False
-        try:
-            print(request.POST)
-            print('dentro do try')
-            novo_cliente = Cliente()           
-            novo_cliente.nome = request.POST['nome']
-            novo_cliente.telefone = request.POST['telefone']
-            novo_cliente.endereco = request.POST['endereco']
-            novo_cliente.numero = request.POST['numero']
-            novo_cliente.cidade = request.POST['cidade']
-            novo_cliente.estado = request.POST['estado']
-            novo_cliente.pais = request.POST['pais']
-            novo_cliente.cep = request.POST['cep']
-            novo_cliente.save()
-            teste = True
-            context['mensagem'] = "Cliente Cadastrado com sucesso."
-            print('try encerrado')
-        except:
-            teste = False
-            print('entrou no except')
-            context['mensagem'] = "Cliente não cadastrado."
-            
-        context['status'] = teste
-        return render(request, 'cliente/listagem.html', context)
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            try:
+                print(request.POST)
+                print('dentro do try')
+                novo_cliente = Cliente()           
+                novo_cliente.nome = request.POST['nome']
+                novo_cliente.telefone = request.POST['telefone']
+                novo_cliente.endereco = request.POST['endereco']
+                novo_cliente.numero = request.POST['numero']
+                novo_cliente.cidade = request.POST['cidade']
+                novo_cliente.estado = request.POST['estado']
+                novo_cliente.pais = request.POST['pais']
+                novo_cliente.cep = request.POST['cep']
+                novo_cliente.save()
+                messages.success(request, "Cliente cadastrado com sucesso.")
+                return redirect('cliente')
+            except:
+                messages.error(request, "Algo deu errado. Tente novamente")
+                return redirect('cadastro-cliente')
 
 
 class DeletarCliente(View):
-
-    def post(self, request):
-        print(request.POST['id'])
-        context = { }
-        teste = False
-        try:
-            cliente = Cliente.objects.get(id=request.POST['id'])
-            print(cliente.nome)
-            cliente.delete()
-            context['mensagem'] = "Cliente removido com sucesso."
-            teste = True
-        except:
-            context['mensagem'] = "Não foi possível remover o cliente, tente novamente."
-            teste = False
-        context['status'] = teste
-        return JsonResponse(context)
+    
+    def post(self, request, pk):
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            try:
+                cliente = Cliente.objects.get(id=pk)
+                print(cliente.nome)
+                cliente.delete()
+                messages.success(request, "Cliente removido com sucesso.")
+            except:
+                messages.error(request, "Algo deu errado. Tente novamente")
+            
+            return redirect('cliente')
 
 
 class EditarCliente(View):
 
-    def post(self, request):
+    def get(self, request, pk):
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            return render(request, 'cliente/editar-cliente.html', {'id' : pk})
 
-        return ''       
+    def post(self, request, pk):
+        if request.user.is_anonymous:
+            return redirect('login')
+        else:
+            try:
+                cliente = Cliente.objects.get(id=pk)
+                cliente.nome = request.POST.get('nome')
+                cliente.telefone = request.POST.get('telefone')
+                cliente.endereco = request.POST.get('endereco')
+                cliente.numero = request.POST.get('numero')
+                cliente.cidade = request.POST.get('cidade')
+                cliente.estado = request.POST.get('estado')
+                cliente.pais = request.POST.get('pais')
+                cliente.cep = request.POST.get('cep')
+                cliente.save()
+                messages.success(request, "Cliente editado com sucesso.")
+            except:
+                messages.error(request, "Algo deu errado. Tente novamente")
+                
+            return redirect('cliente')
 
 class Login(View):
 
@@ -103,7 +124,7 @@ class Login(View):
 
 class Logout(View):
     
-    def post(self, request):
+    def get(self, request):
         logout(request)
         return redirect('login')
 
@@ -114,7 +135,7 @@ class Usuario(View):
         return render(request, 'usuario/cadastrar-usuario.html')
 
     def post(self, request):
-        print('post usuario')        
+             
         try:
             user = User.objects.create_user(request.POST.get('nome'), request.POST.get('email').lower(), request.POST.get('senha'))
             messages.success(request, "Usuário Cadastrado com sucesso.")
